813d8e5debcddf6d4b092eceb7a8da1d9eb505b0
Rafael Escalante <copperx@users.noreply.github.com>
2025-11-24 13:55:07 -0700

    Create arith_utils.h

---
+++ arith_utils.h
@@ -0,0 +1,9 @@
+#ifndef ARITH_UTILS_H
+#define ARITH_UTILS_H
+
+float add(float a, float b);
+float subtract(float a, float b);
+float multiply(float a, float b);
+float divide(float a, float b);
+
+#endif
