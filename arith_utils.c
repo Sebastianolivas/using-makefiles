648f46703ebe2a68215e33e49583220c9bcad777
Rafael Escalante <copperx@users.noreply.github.com>
2025-11-24 13:55:27 -0700

    Create arith_utils.c

---
+++ arith_utils.c
@@ -0,0 +1,20 @@
+#include "arith_utils.h"
+
+// TODO: implement each function
+
+float add(float a, float b) {
+    // TODO
+}
+
+float subtract(float a, float b) {
+    // TODO
+}
+
+float multiply(float a, float b) {
+    // TODO
+}
+
+float divide(float a, float b) {
+    // TODO
+    // if b == 0 return 0.0f
+}
